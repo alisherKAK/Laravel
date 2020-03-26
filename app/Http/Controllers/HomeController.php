61211c08651a0da50731f0eb36b1e59cd6e7d417
alisherKak <alisher.alish333@gmail.com>
2020-03-26 15:38:34 +0600

    Add Book model and CRUD for this model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,10 +8,57 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     function index(Request $request) {
-        return 'index';
+        return view('index', [
+            'books' => Book::all()
+        ]);
     }
 
-    function book(Book $book) {
-        return $book;
+    function details(Book $book) {
+        return view('details', [
+            'book' => $book
+        ]);
+    }
+
+    function create() {
+        return view('form');
+    }
+
+    function createPost(Request $request) {
+        $request->validate($this->rules());
+
+        $book = new Book($request->except('_token'));
+        $book->save();
+
+        return redirect()->route('book.details', $book);
+    }
+
+    function update(Book $book) {
+        return view('form', [
+            'book' => $book
+        ]);
+    }
+
+    function updatePut(Request $request, Book $book) {
+        $rules = $this->rules();
+        $rules['name'] .= ",name,{$book->id}";
+        $request->validate($rules);
+
+        $data = $request->except(['_token', '_method']);
+        $book->update($data);
+        $book->save();
+
+        return redirect()->route('book.details',  $book);
+    }
+
+    function delete(Book $book) {
+        $book->delete();
+        return redirect()->route('index');
+    }
+
+    protected function rules() {
+        return [
+            'name' => 'required|unique:books',
+            'year' => 'required|int'
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
-Route::get('/book/{book}', 'HomeController@book');
+#region Book CRUD
+Route::get('/', 'HomeController@index')
+    ->name('index');
+
+Route::get('/book/create', 'HomeController@create')
+    ->name('book.create');
+
+Route::post('/book', 'HomeController@createPost')
+    ->name('book.create.post');
+
+Route::get('/book/{book}/update', 'HomeController@update')
+    ->name('book.update');
+
+Route::put('/book/{book}', 'HomeController@updatePut')
+    ->name('book.update.put');
+
+Route::delete('/book/{book}', 'HomeController@delete')
+    ->name('book.delete');
+
+Route::get('/book/{book}', 'HomeController@details')
+    ->name('book.details');
+#endregion
+
+#region Music CRUD
+Route::get('/music', 'MusicController@index')
+    ->name('music.index');
+
+Route::get('/music/create', 'MusicController@create')
+    ->name('music.create');
+
+Route::post('/music/create', 'MusicController@createPost')
+    ->name('music.create.post');
+
+Route::get('/music/update/{music}', 'MusicController@update')
+    ->name('music.update');
+
+Route::put('/music/update/{music}', 'MusicController@updatePut')
+    ->name('music.update.put');
+
+Route::delete('/music/delete/{music}', 'MusicController@delete')
+    ->name('music.delete');
+
+Route::get('/music/{music}', 'MusicController@details')
+    ->name('music.details');
+#endregion
